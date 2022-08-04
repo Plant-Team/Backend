3236ff52787f2085ff7e6b7c0925c63c90a89ed8
Rancor38 <zak.rancor@hotmail.com>
2022-08-04 15:55:30 -0700

    Added http methods to users

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,5 +17,46 @@ router.get('/', async(req,res,next) => {
     }
 })
 
+router.post('/', async(req,res,next) => {
+    try{
+        const newUser = await User.create(req.body)
+        res.json(newUser)
+    } catch(err) {
+        next(err)
+    }
+
+})
+
+router.put('/:id', async(req,res,next) => {
+    try{
+        const userUpdated = await User.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new:true}
+        )
+
+        if(userUpdated) {
+            res.json(userUpdated)
+        } else {
+            res.sendStatus(404)
+        }
+    } catch(err) {
+        next(err)
+    }
+})
+
+
+router.delete('/:id', async(req,res,next) => {
+    try{
+        const deleteUser = await User.findByIdAndDelete(req.params.id)
+        if(deleteUser) {
+            res.json(deleteUser)
+        } else {
+            res.sendStatus(404)
+        }
+    } catch(err) {
+        next(err)
+    }
+})
 
 module.exports = router
