03825ea4b8d850d036f62a742cd6954fe8dd602a
brianthoeurn <brianmonirath@hotmail.com>
2022-08-10 07:34:33 -0700

    added middleware session

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const passport = require("passport");
 const session = require("express-session");
 const dotenv = require('dotenv').config()
+const MongoDBSession = require('connect-mongodb-session')(session)
 
 const app = express();
 require("./db/connection");
@@ -11,26 +12,31 @@ app.set("port", process.env.PORT || 4000);
 //=============================================================================
 // Middleware
 //=============================================================================
-const SESSION_SECRET = process.env.SESSION_SECRET;
-
+const mongoURI = process.env.DATABASE_URL;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname))
+app.use(cors());
+const store = new MongoDBSession({
+  uri: mongoURI, 
+  collection: 'my sessions'
+})
 // secret: What we actually will be giving the user on our site as a session cookie
 // resave: Save the session even if it's modified, make this false
 // saveUninitialized: If we have a new session, we save it, therefore making that true
-const sessionObject = {
-  secret: SESSION_SECRET,
+app.use(session({
+  secret: 'cookie for coin?',
   resave: false,
   saveUninitialized: true,
-};
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
-app.use(session(sessionObject));
+  store: store,
+}));
+
 app.use(passport.initialize());
 // Add a session
 app.use(passport.session())
 
-
 app.get("/", (req, res) => {
+  console.log(req.session)
   res.redirect("/api/plants");
 });
 
